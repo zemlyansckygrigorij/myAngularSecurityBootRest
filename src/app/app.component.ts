8f407b7da63cd0803e3de93a5f11d7a2b5d6820c
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2018-02-05 11:32:27 +0400

    new change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+declare var jquery: any;
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import {UserComponent} from './user/user.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {AppUserService} from './shared/user.service';
+import {LoginComponent} from './security/login/login.component';
+import {AuthGuard} from './security/guards/auth.guards';
+import {AuthService} from './security/service/auth.service';
+import {RouterModule} from '@angular/router';
+import {HeaderComponent} from './security/header/header.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HttpHandler} from './util/httpHandler';
+import {HttpClient, HttpHeaders, HttpClientModule} from '@angular/common/http';
 
-
+const routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    DashboardComponent,
+    UserListComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // FormBuilder,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    HttpModule,
+    HttpClientModule, // Импортируем модуль
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [AppUserService,
+    AuthGuard,
+    AuthService,
+    HttpHandler],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
